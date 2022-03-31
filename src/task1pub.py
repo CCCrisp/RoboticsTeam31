21be1a49b641b6f344ce665508efeda0b47f962f
Yifeng Liu <yliu356@sheffield.ac.uk>
2022-03-31 19:36:37 +0100

    Change the template of publisher

--- src/task1pub.py
+++ src/task1pub.py
@@ -1,47 +1,108 @@
 #!/usr/bin/env python3
-# A simple ROS publisher node in Python
 
 import rospy
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from math import sqrt, pow, pi
 
-class Publisher():
-    
+class Square:
+    def callback_function(self, odom_data):
+        # obtain the orientation and position co-ords:
+        or_x = odom_data.pose.pose.orientation.x
+        or_y = odom_data.pose.pose.orientation.y
+        or_z = odom_data.pose.pose.orientation.z
+        or_w = odom_data.pose.pose.orientation.w
+        pos_x = odom_data.pose.pose.position.x
+        pos_y = odom_data.pose.pose.position.y
+
+        # convert orientation co-ords to roll, pitch & yaw (theta_x, theta_y, theta_z):
+        (roll, pitch, yaw) = euler_from_quaternion([or_x, or_y, or_z, or_w], 'sxyz')
+        
+        self.x = pos_x
+        self.y = pos_y
+        self.theta_z = yaw 
+
+        if self.startup:
+            self.startup = False
+            self.x0 = self.x
+            self.y0 = self.y
+            self.theta_z0 = self.theta_z
+
     def __init__(self):
-        self.node_name = "move_circle"
-        topic_name = "cmd_vel"
+        node_name = "move_square"
+        
+        self.startup = True
+        self.turn = False
 
-        self.pub = rospy.Publisher(topic_name, Twist, queue_size=10)
-        rospy.init_node(self.node_name, anonymous=True)
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.sub = rospy.Subscriber('odom', Odometry, self.callback_function)
+
+        rospy.init_node(node_name, anonymous=True)
         self.rate = rospy.Rate(10) # hz
+
+        self.x = 0.0
+        self.y = 0.0
+        self.theta_z = 0.0
+        self.x0 = 0.0
+        self.y0 = 0.0
+        self.theta_z0 = 0.0
         
-        self.vel_cmd = Twist()
-        
+        self.vel = Twist()
+
         self.ctrl_c = False
-        rospy.on_shutdown(self.shutdownhook) 
-        
-        rospy.loginfo(f"The '{self.node_name}' node is active...")
+        rospy.on_shutdown(self.shutdownhook)
+
+        rospy.loginfo(f"the {node_name} node has been initialised...")
 
     def shutdownhook(self):
-        self.vel_cmd.linear.x = 0.0 # m/s
-        self.vel_cmd.angular.z = 0.0 # rad/s
-        self.pub.publish(self.vel_cmd)
-        print(f"Stopping the '{self.node_name}' node at: {rospy.get_time()}")
+        self.pub.publish(Twist())
         self.ctrl_c = True
+    
+    def print_stuff(self, a_message):
+        print(a_message)
+        print(f"current velocity: lin.x = {self.vel.linear.x:.1f}, ang.z = {self.vel.angular.z:.1f}")
+        print(f"current odometry: x = {self.x:.3f}, y = {self.y:.3f}, theta_z = {self.theta_z:.3f}")
 
     def main_loop(self):
+        status = ""
+        wait = 0
         while not self.ctrl_c:
-            publisher_message = f"rospy time is: {rospy.get_time()}"
-            path_rad = 0.5 # m
-            lin_vel = 0.1 # m/s
-            
-            self.vel_cmd.linear.x = lin_vel
-            self.vel_cmd.angular.z = lin_vel / path_rad # rad/s
-            self.pub.publish(self.vel_cmd)
+            if self.startup:
+                self.vel = Twist()
+                status = "init"
+            elif self.turn:
+                if abs(self.theta_z0 - self.theta_z) >= pi/2 and wait > 5:
+                    # If the robot has turned 90 degrees (in radians) then stop turning
+                    self.turn = False
+                    self.vel = Twist()
+                    self.theta_z0 = self.theta_z
+                    status = "turn-fwd transition"
+                    wait = 0
+                else:
+                    self.vel = Twist()
+                    self.vel.angular.z = 0.2
+                    status = "turning"
+                    wait += 1
+            else:
+                if sqrt(pow(self.x0 - self.x, 2) + pow(self.y0 - self.y, 2)) >= 0.5:
+                    # if distance travelled is greater than 0.5m then stop, and start turning:
+                    self.vel = Twist()
+                    self.turn = True
+                    self.x0 = self.x
+                    self.y0 = self.y
+                    status = "fwd-turn transition"
+                else:
+                    self.vel = Twist()
+                    self.vel.linear.x = 0.1
+                    status = "moving forwards"
+            self.pub.publish(self.vel)
+            self.print_stuff(status)
             self.rate.sleep()
 
 if __name__ == '__main__':
-    publisher_instance = Publisher()
+    movesquare_instance = Square()
     try:
-        publisher_instance.main_loop()
+        movesquare_instance.main_loop()
     except rospy.ROSInterruptException:
         pass
